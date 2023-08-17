a78a67c61f8da0414068fa2b49db1c68db7d540d
jimin song <songing01@gmail.com>
2023-08-17 22:18:28 +0900

    🐛 라우팅 오류 수정

--- src/App.js
+++ src/App.js
@@ -8,13 +8,15 @@ import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <Routes>
+    <>
       <Header />
-      <Route path="mypage/reservation" element={<MyReservePage />} />
-      <Route path="mypage/reservation/:id" element={<DetailReservePage />} />
-      <Route path="/photograph-reserve" element={<Reservation />} />
-      <Route path="/photograph-reserve/:id" element={<ReservationDetail />} />
-    </Routes>
+      <Routes>
+        <Route path="mypage/reservation" element={<MyReservePage />} />
+        <Route path="mypage/reservation/:id" element={<DetailReservePage />} />
+        <Route path="/photograph-reserve" element={<Reservation />} />
+        <Route path="/photograph-reserve/:id" element={<ReservationDetail />} />
+      </Routes>
+    </>
   );
 }
 
