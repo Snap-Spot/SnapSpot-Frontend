3c83ff177149de3838c5c3ae699c381dfe2e8b01
wowalswjd <alswjd5491@naver.com>
2023-08-19 00:59:11 +0900

    :lipstick: 회원가입 멤버 선택, 정보 입력 페이지 퍼블리싱

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import Header from "./components/common/Header";
 import ReservationDetail from "./pages/photographer/ReservationDetail";
 import Reservation from "./pages/photographer/Reservation";
 import { Routes, Route } from "react-router-dom";
+import SignUpInfoPage from "./pages/SignUpInfoPage";
+import SignUpMemberPage from "./pages/SignUpMemberPage";
 
 function App() {
   return (
     <>
       <Header />
       <Routes>
+        {/* 회원가입 */}
+        <Route path="/signup/member" element={<SignUpMemberPage />} />
+        <Route path="/signup/:memberType/info" element={<SignUpInfoPage />} />
         <Route path="/photograph-reserve" element={<Reservation />} />
         <Route path="/photograph-reserve/:id" element={<ReservationDetail />} />
       </Routes>
---
+++ src/pages/SignUpMemberPage.js
@@ -0,0 +1,115 @@
+import React from "react";
+import { styled } from "styled-components";
+import Customer from "../assets/signup/customer.png";
+import Photographer from "../assets/signup/photographer.png";
+import M_Customer from "../assets/signup/customer_mobile.png";
+import M_Photographer from "../assets/signup/photographer_mobile.png";
+import { useNavigate } from "react-router";
+
+const SignUpMemberPage = () => {
+  const navigate = useNavigate();
+
+  // 정보 입력 페이지로 이동
+  const navigateToInfoPage = (memberType) => {
+    navigate(`/signup/${memberType}/info`);
+  };
+
+  return (
+    <Wrapper>
+      <div className="container">
+        <MainText>회원가입 유형을 골라주세요!</MainText>
+        <MainDiv>
+          <MemberDiv>
+            <MemberImage
+              src={window.innerWidth < 768 ? M_Customer : Customer}
+              alt="고객"
+              onClick={() => {
+                navigateToInfoPage("customer");
+              }}
+            />
+            <MemberText color="#008EDE">인생사진을 찍고 싶어요!</MemberText>
+          </MemberDiv>
+          <MemberDiv>
+            <MemberImage
+              src={window.innerWidth < 768 ? M_Photographer : Photographer}
+              alt="작가"
+              onClick={() => {
+                navigateToInfoPage("photographer");
+              }}
+            />
+            <MemberText color="#3C3AAC">멋진 사진을 찍어드릴게요!</MemberText>
+          </MemberDiv>
+        </MainDiv>
+      </div>
+    </Wrapper>
+  );
+};
+
+export default SignUpMemberPage;
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  @media screen and (max-width: 768px) {
+    height: 100vh;
+  }
+`;
+const MainText = styled.div`
+  font-family: Noto Sans KR;
+  font-size: 1.065rem;
+  font-weight: 600;
+
+  margin-bottom: 60px;
+
+  margin-top: 40px;
+
+  @media screen and (max-width: 768px) {
+    margin-top: 0;
+  }
+`;
+
+const MainDiv = styled.div`
+  display: flex;
+  gap: 40px;
+
+  @media screen and (max-width: 768px) {
+    gap: 15px;
+  }
+`;
+
+const MemberDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const MemberImage = styled.img`
+  width: 215px;
+  cursor: pointer;
+
+  &:hover {
+    filter: brightness(0.7);
+  }
+
+  @media screen and (max-width: 768px) {
+    width: 130px;
+  }
+`;
+
+const MemberText = styled.span`
+  font-family: Noto Sans KR;
+  font-size: 1.065rem;
+  font-weight: 500;
+
+  text-align: center;
+  color: ${(props) => props.color};
+
+  margin-top: 30px;
+
+  @media screen and (max-width: 768px) {
+    font-size: 0.75rem;
+  }
+`;
