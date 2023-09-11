728128889f15fd2d6f1d96040bf525144ef53726
wowalswjd <alswjd5491@naver.com>
2023-09-12 00:34:46 +0900

    :art: 카카오로 회원가입 버튼 따로 생성

--- src/components/Login/KakaoLoginBtn.js
+++ src/components/Login/KakaoLoginBtn.js
@@ -4,11 +4,15 @@ import { S } from "../common/SignUpLoginBtn.style";
 import kakaoLogo from "../../assets/signup/kakao.png";
 
 const KakaoLoginBtn = (props) => {
+
   const CLIENT_MAIN_URL = process.env.REACT_APP_REACT_URL;
 
   const REST_API_KEY = process.env.REACT_APP_REST_API_KEY;
 
-  const REDIRECT_URI = encodeURI(`${CLIENT_MAIN_URL}/auth/kakao-login`);
+  // props.formType - "signup" 또는 "login"
+  const REDIRECT_URI = encodeURI(
+    `${CLIENT_MAIN_URL}/auth/kakao-${props.formType}`
+  );
 
   const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 
@@ -19,7 +23,7 @@ const KakaoLoginBtn = (props) => {
   return (
     <S.KakaoLoginBtn onClick={handleKakaoLogin} className={props.formType}>
       <img src={kakaoLogo} alt="kakao" />
-      <p>카카오로 로그인</p>
+      <p>카카오로 {props.formType === "signup" ? "시작하기" : "로그인"}</p>
     </S.KakaoLoginBtn>
   );
 };
