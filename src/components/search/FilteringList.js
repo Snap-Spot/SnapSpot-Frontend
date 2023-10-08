af7655f90d720b32cfaeb30fcad80f8c1af6daa8
jiwoorld <bncmdbev@gmail.com>
2023-10-08 22:30:55 +0900

    🚑 카테고리 오타 수정

--- src/components/search/FilteringList.js
+++ src/components/search/FilteringList.js
@@ -265,7 +265,7 @@ export const orders = [
 
 export const category = [
   { key: "COUPLE", label: "커플스냅" },
-  { key: "FRIENDSHIP", label: "우정스냅" },
+  { key: "FRIEND", label: "우정스냅" },
   { key: "GRADUATION", label: "졸업스냅" },
   { key: "WEDDING", label: "웨딩스냅" },
   { key: "FAMILY", label: "가족스냅" },
